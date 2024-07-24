0fff3c08c3ec5c6baa9428b92bac8f8b7ccd7f93
Resul Avan <resul.avan@ing.com>
2024-07-24 23:45:59 +0200

    simplified about page and country in profile summary

--- components/profile/header/ProfileSummary.vue
+++ components/profile/header/ProfileSummary.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {AlbumType, UserProfile} from "~/types";
 import {getDisplayName, getProfilePhotoImage} from "~/service/user-profile-service";
+import {Country} from "country-state-city";
 
 const props = defineProps<{
   userProfile: UserProfile
@@ -8,7 +9,8 @@ const props = defineProps<{
 }>()
 const {getAlbumImagesByAlbumId} = useAlbumImageCollection()
 const displayName = computed(() => getDisplayName(props.userProfile))
-const profilePhoto = computed(() => getProfilePhotoImage(props?.userProfile,displayName.value))
+const profilePhoto = computed(() => getProfilePhotoImage(props?.userProfile, displayName.value))
+const country = ref(props.userProfile?.address?.country && Country.getCountryByCode(props.userProfile.address.country))
 const showProfileLightbox = ref(false)
 const profileAlbumImages = ref([])
 const currentProfileImageIndex = ref(0)
@@ -34,7 +36,8 @@ const loadProfileImages = () => {
       class="-mt-28 mx-auto flex flex-col items-center w-full px-4 absolute sm:w-auto sm:space-x-2 sm:flex-row sm:px-6 lg:px-8">
     <div class="bg-gray-400 rounded-full h-52 w-52">
       <NuxtImg class="object-cover rounded-full h-52 w-52 border-solid border-2 border-gray-300 dark:border-gray-900"
-              :class="{'cursor-pointer': userProfile?.profilePhoto?.id}" :src="profilePhoto.src" :alt="profilePhoto.alt" @click="loadProfileImages" placeholder/>
+               :class="{'cursor-pointer': userProfile?.profilePhoto?.id}" :src="profilePhoto.src"
+               :alt="profilePhoto.alt" @click="loadProfileImages" placeholder/>
       <client-only>
         <div v-if="isMyProfile" class="absolute text-2xl -my-9 mx-40 opacity-100">
           <UploadButton :album-type="AlbumType.PROFILE"/>
@@ -43,13 +46,19 @@ const loadProfileImages = () => {
                   :albumImages="profileAlbumImages"/>
       </client-only>
     </div>
-    <div class="mt-2 sm:mt-20">
+    <div class="mt-2 sm:mt-24">
       <div class="flex flex-col text-center sm:text-left sm:space-y-0">
         <div v-if="displayName">
           <span class="truncate text-xl font-extrabold">{{ displayName }}</span>
         </div>
         <div>
           <span class="truncate">@{{ userProfile.username }}</span>
+        </div>
+        <div v-if="country">
+          <div class="flex flex-row items-center">
+            <div class="text-xl mr-1">{{ country.flag }}</div>
+            <div>{{ country.name }}</div>
+          </div>
         </div>
       </div>
     </div>
--- components/profile/module/AboutProfile.vue
+++ components/profile/module/AboutProfile.vue
@@ -1,63 +1,23 @@
 <script setup lang="ts">
 import {UserProfile} from "~/types";
-import {getDisplayName} from "~/service/user-profile-service";
-import {Country} from "country-state-city";
 
-const props = defineProps<{
+defineProps<{
   userProfile: UserProfile
   isMyProfile?: boolean
 }>()
 const {t} = useI18n()
 
-const displayName = computed(() => {
-  return getDisplayName(props.userProfile)
-})
-
-const country = ref(props.userProfile?.address?.country && Country.getCountryByCode(props.userProfile.address.country))
-
 </script>
 
 <template>
   <UContainer>
-
     <UDashboardPanelContent>
-
-      <div class="grid grid-cols-3 gap-4">
-        <div class="col-span-1">{{t('field.fullName.label')}}</div>
-        <div>{{ displayName }}</div>
-      </div>
-
-      <UDashboardSection :title="t('field.fullName.label')" orientation="horizontal"
-                         :ui="{container: 'grid grid-cols-3 gap-4'}">
-        <!--        <template #links>-->
-        <div class="flex justify-center items-center">
-          <span>{{ displayName }}</span>
-        </div>
-        <!--        </template>-->
-      </UDashboardSection>
-
-      <UDashboardSection :title="t('field.country.label')" orientation="horizontal"
-                         :ui="{container: 'flex items-center justify-center sm:justify-between sm:flex-row gap-4'}">
-        <!--        <template #links>-->
-        <UCard class="flex items-center justify-center">
-          <div class="flex flex-row items-center">
-            <span class="text-2xl mr-2">{{ country.flag }}</span>
-            <span>{{ country.name }}</span>
-          </div>
-        </UCard>
-        <!--        </template>-->
-      </UDashboardSection>
-
       <UDashboardSection :title="t('field.about.label')" orientation="horizontal"
                          :ui="{container: 'flex items-center justify-center sm:justify-between sm:flex-row gap-4'}">
-        <!--        <template #links>-->
         <UCard>
           <span>{{ userProfile.about }}</span>
         </UCard>
-
-        <!--        </template>-->
       </UDashboardSection>
-
     </UDashboardPanelContent>
   </UContainer>
 </template>
