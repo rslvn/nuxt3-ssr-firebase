6a4609e4ca5299a8d9160700e76dd773c286fd65
Resul Avan <resul.avan@ing.com>
2024-07-24 22:01:23 +0200

    remove useAuthUser composable

--- stores/auth-store.ts
+++ stores/auth-store.ts
@@ -1,6 +1,8 @@
 import {defineStore} from 'pinia'
 import {AuthState, AuthUser} from "~/types";
 
+const {getAuthUserByCookie, getAuthUserByHeader, logout} = useAuthApi()
+
 export const useAuthStore = defineStore('auth-store', {
     state: (): AuthState => {
         return {
@@ -11,5 +13,17 @@ export const useAuthStore = defineStore('auth-store', {
         setAuthUser(authUser: AuthUser) {
             this.authUser = authUser
         },
+        async setAuthUserByCookie() {
+            const authUser = await getAuthUserByCookie()
+            this.setAuthUser(authUser)
+        },
+        async setAuthUserByHeader(token: string) {
+            const authUser = await getAuthUserByHeader(token)
+            this.setAuthUser(authUser)
+        },
+        async removeAuthUser() {
+            await logout()
+            this.setAuthUser(null)
+        }
     }
 })
